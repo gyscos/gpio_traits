f748207650ec97066c6a4466603f94665677aa70
Alexandre Bury <alexandre.bury@gmail.com>
2017-01-13 17:15:18 -0800

    Remove Slave Select from BigBangingSerial
    It belongs somewhere else.
    It is relevant for any SPI bus, not just bit banging ones.

--- src/spi.rs
+++ src/spi.rs
@@ -1,9 +1,9 @@
 use pin;
 
-// TODO: handle baudrate configuration?
-// TODO: handle read?
 /// A SPI device
 pub trait Serial {
+    // TODO: write bits?
+    // Write &[u8]?
     /// Simultaneously read and write.
     fn write(&mut self, data: u8) -> u8;
 }
@@ -13,7 +13,9 @@ pub struct DummySerial;
 
 impl Serial for DummySerial {
     /// Ignores input and always return `0`.
-    fn write(&mut self, _: u8) -> u8 { 0 }
+    fn write(&mut self, _: u8) -> u8 {
+        0
+    }
 }
 
 
@@ -30,35 +32,32 @@ impl Serial for DebugSerial {
     }
 }
 
+// TODO: handle baudrate configuration?
 // TODO: handle sleep between clock cycles
+// TODO: better slave select
 /// A bit-banging implementation of SPI on gpio pins.
-pub struct BitBangingSerial<SCK, SS, MOSI, MISO>
+pub struct BitBangingSerial<SCK, MOSI, MISO>
     where SCK: pin::Output,
-          SS: pin::Output,
           MOSI: pin::Output,
           MISO: pin::Input
 {
     sck: SCK,
-    ss: SS,
     mosi: MOSI,
     miso: MISO,
 }
 
-impl<SCK, SS, MOSI, MISO> BitBangingSerial<SCK, SS, MOSI, MISO>
+impl<SCK, MOSI, MISO> BitBangingSerial<SCK, MOSI, MISO>
     where SCK: pin::Output,
-          SS: pin::Output,
           MOSI: pin::Output,
-          MISO: pin::Input,
-
+          MISO: pin::Input
 {
     /// Creates a bit-banging SPI device using the given pins.
     ///
     /// * `mosi`: Master Out, Slave In
     /// * `miso`: Master In, Slave Out
-    pub fn new(sck: SCK, ss: SS, mosi: MOSI, miso: MISO) -> Self {
+    pub fn new(sck: SCK, mosi: MOSI, miso: MISO) -> Self {
         BitBangingSerial {
             sck: sck,
-            ss: ss,
             mosi: mosi,
             miso: miso,
         }
@@ -67,30 +66,29 @@ impl<SCK, SS, MOSI, MISO> BitBangingSerial<SCK, SS, MOSI, MISO>
     fn write_bit(&mut self, bit: bool) -> pin::PinState {
         self.sck.low();
         self.mosi.write(bit.into());
+        // DELAY
         self.sck.high();
         self.miso.read()
+        // DELAY?
     }
 }
 
-impl<SCK, SS, MOSI, MISO> Serial for BitBangingSerial<SCK, SS, MOSI, MISO>
+impl<SCK, MOSI, MISO> Serial for BitBangingSerial<SCK, MOSI, MISO>
     where SCK: pin::Output,
-          SS: pin::Output,
           MOSI: pin::Output,
-          MISO: pin::Input,
+          MISO: pin::Input
 {
     fn write(&mut self, data: u8) -> u8 {
-        self.ss.low();
 
         let result = ((self.write_bit((data & 0b10000000) != 0) as u8) << 7) |
-        ((self.write_bit((data & 0b01000000) != 0) as u8) << 6) |
-        ((self.write_bit((data & 0b00100000) != 0) as u8) << 5) |
-        ((self.write_bit((data & 0b00010000) != 0) as u8) << 4) |
-        ((self.write_bit((data & 0b00001000) != 0) as u8) << 3) |
-        ((self.write_bit((data & 0b00000100) != 0) as u8) << 2) |
-        ((self.write_bit((data & 0b00000010) != 0) as u8) << 1) |
-        (self.write_bit((data & 0b00000001) != 0) as u8);
+                     ((self.write_bit((data & 0b01000000) != 0) as u8) << 6) |
+                     ((self.write_bit((data & 0b00100000) != 0) as u8) << 5) |
+                     ((self.write_bit((data & 0b00010000) != 0) as u8) << 4) |
+                     ((self.write_bit((data & 0b00001000) != 0) as u8) << 3) |
+                     ((self.write_bit((data & 0b00000100) != 0) as u8) << 2) |
+                     ((self.write_bit((data & 0b00000010) != 0) as u8) << 1) |
+                     (self.write_bit((data & 0b00000001) != 0) as u8);
 
-        self.ss.high();
 
         result
     }
